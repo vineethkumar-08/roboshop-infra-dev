368c8d6e60940e3c0adc7a2dee4594369a697042
Vineethk08 <milatariv@gmail.com>
2026-03-17 16:27:42 -0400

    redis

--- 40-databases/r53.tf
+++ 40-databases/r53.tf
@@ -1,7 +1,17 @@
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "mongodb" {
   zone_id = var.zone_id
   name    = "mongodb-${var.environment}.${var.domain_name}"
   type    = "A"
   ttl     = 1
   records = [aws_instance.mongodb.private_ip]
+  allow_overwrite = true
+} 
+
+resource "aws_route53_record" "redis" {
+  zone_id = var.zone_id
+  name    = "redis-${var.environment}.${var.domain_name}"
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.redis.private_ip]
+  allow_overwrite = true
 } 
