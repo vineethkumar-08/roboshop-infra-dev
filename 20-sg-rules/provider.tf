a1e55a312cc4cb357ff340a23924fed1a1c2c9a4
Vineethk08 <milatariv@gmail.com>
2026-03-17 05:12:58 -0400

    ansible

--- 20-sg-rules/provider.tf
+++ 20-sg-rules/provider.tf
@@ -4,15 +4,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.33.0" # terrafomr aws provider version
     }
-    http = {
-      source  = "hashicorp/http"
-      version = "3.4.5"
-    }
   }
   
   backend "s3" {
     bucket         = "remote-state-vineeth-demo"
-    key            = "roboshop-dev-sg-rules"
+    key            = "roboshop-sg-rules"
     region         = "us-east-1"
     encrypt        = true
     use_lockfile = true
