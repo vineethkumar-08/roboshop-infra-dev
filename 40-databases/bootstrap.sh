6cceb8d4381b28693ad4883adcff3de77d69b973
Vineethk08 <milatariv@gmail.com>
2026-03-17 03:51:23 -0400

    ansible

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-
+component=$1
 dnf install ansible -y
+
+cd /home/ec2-user
+git clone https://github.com/vineethkumar-08/ansible-roboshop-roles-tf.git
+
+cd ansible-roboshop-roles-tf
+
+ansible-playbook -e component=$component roboshop.yaml
--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -37,7 +37,7 @@ resource "terraform_data" "bootstrap" {
   provisioner "remote-exec" {
     inline = [
       "chmod +x /tmp/bootstrap.sh",
-      "sudo sh /tmp/bootstrap.sh"
+      "sudo sh /tmp/bootstrap.sh mongodb"
     ]
   }
 }
