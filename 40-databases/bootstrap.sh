8e7a204e63633e3812be3045447f04d763041cfe
Vineethk08 <milatariv@gmail.com>
2026-03-17 22:47:50 -0400

    fix: rename mysql-lam-policy.json to mysql-iam-policy.json

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 component=$1
+component=$2
 dnf install ansible -y
 
 cd /home/ec2-user
@@ -8,4 +9,4 @@ git clone https://github.com/vineethkumar-08/ansible-roboshop-roles-tf.git
 cd ansible-roboshop-roles-tf
 git pull
 
-ansible-playbook -e component=$component roboshop.yaml
+ansible-playbook -e component=$component -e environment=$environment roboshop.yaml
