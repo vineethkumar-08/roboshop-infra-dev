2e8a2ac025a0e54b3ad5706ede3250fb0f6b29c1
Vineethk08 <milatariv@gmail.com>
2026-03-17 21:42:05 -0400

    git pull request for ansible boto issue

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -11,7 +11,7 @@ locals {
     redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
     mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
     mysql_role_name = join("-" ,[
-      for name in ["${var.project}","${var.environment}", "mysql"] : tittle(name)
+      for name in ["${var.project}","${var.environment}", "mysql"] : title(name)
       ])
 
 }
