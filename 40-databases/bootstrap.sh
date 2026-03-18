db269b0c8eb7ed20d0c6125edb4e32cad307c7c5
Vineethk08 <milatariv@gmail.com>
2026-03-17 23:55:41 -0400

    roles update

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+
 component=$1
-component=$2
+environment=$2
 dnf install ansible -y
 
 cd /home/ec2-user
 git clone https://github.com/vineethkumar-08/ansible-roboshop-roles-tf.git
+
 
 cd ansible-roboshop-roles-tf
 git pull
