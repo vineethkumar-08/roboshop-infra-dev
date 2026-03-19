3cd05fec83e9984c22bd17b9d6e8dddf5b7506af
Vineethk08 <milatariv@gmail.com>
2026-03-19 02:38:30 -0400

    catalogue sg

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -78,3 +78,14 @@ resource "aws_security_group_rule" "backend_alb_bastion" {
   source_security_group_id = local.bastion_sg_id
   security_group_id = local.backend_alb_sg_id
 }
+
+
+resource "aws_security_group_rule" "catalogue_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  # where source of traffic is comming from = its comming from bastian
+  source_security_group_id = local.catalogue_sg_id
+  security_group_id = local.catalogue_sg_id
+}
