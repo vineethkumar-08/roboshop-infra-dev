e5e8ef879b755e18f021db02d35bd7ac37f9206d
Vineethk08 <milatariv@gmail.com>
2026-03-25 15:12:28 -0400

    action

--- 90-components/variable.tf
+++ 90-components/variable.tf
@@ -5,21 +5,21 @@ variable "component" {
     catalogue = {
         rule_priority = 10
     }
-    user = {
-        rule_priority = 20 
-    }
+    # user = {
+    #     rule_priority = 20 
+    # }
     
-    cart = {
-        rule_priority = 30
-    }
+    # cart = {
+    #     rule_priority = 30
+    # }
 
-    shipping ={
-        rule_priority = 40
-    }
+    # shipping ={
+    #     rule_priority = 40
+    # }
 
-    payment = {
-        rule_priority = 50
-    }
+    # payment = {
+    #     rule_priority = 50
+    # }
     #frontend is attaching to frontend_alb,there is only one component there
     frontend = {
         rule_priority = 10
