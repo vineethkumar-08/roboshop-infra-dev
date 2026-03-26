b7d4e114a3b9311dfced2679fb6b786057ba5fc9
Vineethk08 <milatariv@gmail.com>
2026-03-26 00:13:00 -0400

    sg rules modified

--- 90-components/variable.tf
+++ 90-components/variable.tf
@@ -5,13 +5,13 @@ variable "component" {
     catalogue = {
         rule_priority = 10
     }
-    # user = {
-    #     rule_priority = 20 
-    # }
+    user = {
+        rule_priority = 20 
+    }
     
-    # cart = {
-    #     rule_priority = 30
-    # }
+    cart = {
+        rule_priority = 30
+    }
 
     # shipping ={
     #     rule_priority = 40
